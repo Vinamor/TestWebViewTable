94f5bb231b1ddcaf4a17efe26d11685a02c29c60
Vinamor <romanivost@gmail.com>
2017-03-23 13:14:27 +0200

    Solved the problem with main bug
    Each cell is now presenting correct data, each table is presenting
    relevant post!

--- TestWebViewTable/WebViewTableViewController.swift
+++ TestWebViewTable/WebViewTableViewController.swift
@@ -164,12 +164,6 @@ class WebViewTableViewController: UITableViewController {
         
         self.downloadJSONWithURL()
         self.downloadJSONWithURL2()
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-        
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
     
     @IBAction func segmentControlChanged(_ sender: UISegmentedControl) {
@@ -220,9 +214,6 @@ class WebViewTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "newsCell", for: indexPath) as! NewsTableViewCell
         
-        // tableView.register(UINib(nibName: "WebViewTableViewCell", bundle: nil), forCellReuseIdentifier: "newsCell")
-        // tableView.register(WebViewTableViewCell.self, forCellReuseIdentifier: "newsCell")
-        
         switch (segmentControl.selectedSegmentIndex) {
         case 0:
             let post = PostsArray[indexPath.row]
@@ -240,18 +231,6 @@ class WebViewTableViewController: UITableViewController {
                 cell.contentImageView.clipsToBounds = true
             }
             
-//            let imageURL = NSURL(string: (post?.imageStr)!)
-//            
-//            if imageURL != nil {
-//                let data = NSData(contentsOf: (imageURL as? URL)!)
-//                
-//                cell.contentImageView.image = UIImage(data: data as! Data)
-//                cell.contentImageView.layer.cornerRadius = 0.5 * cell.contentImageView.frame.size.width
-//                cell.contentImageView.layer.borderColor = UIColor.darkGray.cgColor
-//                cell.contentImageView.layer.borderWidth = 2.0
-//                cell.contentImageView.clipsToBounds = true
-//            }
-        
         case 1:
             let post2 = PostsArray2[indexPath.row]
             cell.contentTitleLabel.text = post2?.title
@@ -275,79 +254,27 @@ class WebViewTableViewController: UITableViewController {
         return cell
     }
     
-    
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
-    
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
-    
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-     
-     }
-     */
-    
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
-    
-    
     //MARK: - Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
         super.prepare(for: segue, sender: sender)
+        
         if segue.identifier == "showDetail" {
-            
+            let selectedPost: Post?
             if let postDetailViewController = segue.destination as? PostViewController {
                 if let selectedPostCell = sender as? NewsTableViewCell {
                     let indexPath = tableView.indexPath(for: selectedPostCell)!
-                    let selectedPost = PostsArray[indexPath.row]
+                    
+                    switch (segmentControl.selectedSegmentIndex) {
+                    case 0:
+                        selectedPost = PostsArray[indexPath.row]
+                    case 1: selectedPost = PostsArray2[indexPath.row]
+                    default:
+                        fatalError("Unexpected Segue Identifier; \(segue.identifier)")
+                    }
                     postDetailViewController.post = selectedPost
-                } else {
-                    fatalError("Unexpected Segue Identifier; \(segue.identifier)")
-                }
-            }
-            
-            if let postDetailViewController2 = segue.destination as? PostViewController {
-                if let selectedPostCell = sender as? NewsTableViewCell {
-                    let indexPath2 = tableView.indexPath(for: selectedPostCell)!
-                    let selectedPost2 = PostsArray2[indexPath2.row]
-                    postDetailViewController2.post = selectedPost2
-                } else {
-                    fatalError("Unexpected Segue Identifier; \(segue.identifier)")
                 }
             }
         }
     }
-    
-//    @IBAction func unwindToMealList(sender: UIStoryboardSegue) {
-//        if let sourceViewController = sender.source as? PostViewController, let meal = sourceViewController.post {
-//            
-//            if let selectedIndexPath = tableView.indexPathForSelectedRow {
-//                // Update an existing meal.
-//                PostsArray[selectedIndexPath.row] = meal
-//                tableView.reloadRows(at: [selectedIndexPath], with: .none)
-//            }
-//         
-//        }
-//    }
 }
